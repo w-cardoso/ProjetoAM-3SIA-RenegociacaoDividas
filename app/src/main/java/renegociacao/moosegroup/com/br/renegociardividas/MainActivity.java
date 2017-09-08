06465296638ef4f9f110537a5d673e4966332574
Weverton dos Santos Cardoso <re034850@resource.com.br>
2017-09-08 11:22:19 -0300

    App Renegociar Dividas

---
+++ app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/MainActivity.java
@@ -0,0 +1,65 @@
+package renegociacao.moosegroup.com.br.renegociardividas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar ab = getSupportActionBar();
+        ab.hide();
+
+        final TextInputLayout tilCpf = (TextInputLayout) findViewById(R.id.main_til_cpf);
+
+        final EditText edtCpf = (EditText) findViewById(R.id.main_edt_cpf);
+        edtCpf.addTextChangedListener(Mask.insert(Mask.CPF_MASK, edtCpf));
+
+        Button btnEntrar = (Button) findViewById(R.id.main_btn_entrar);
+        TextView txtCadastrar = (TextView) findViewById(R.id.main_txt_cadastrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean cpfValido = Validator.validateCPF(edtCpf.getText().toString().trim());
+                if (!cpfValido) {
+                    tilCpf.setError("Digite um CPF válido");
+                    requestFocus(edtCpf);
+                }else{
+                    tilCpf.setErrorEnabled(false);
+                }
+
+            }
+        });
+
+        txtCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaCadastrar = new Intent(MainActivity.this, TelaCadastroActivity.class);
+                startActivity(telaCadastrar);
+            }
+        });
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+
+}
+
---
+++ app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/TelaCadastroActivity.java
@@ -0,0 +1,162 @@
+package renegociacao.moosegroup.com.br.renegociardividas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class TelaCadastroActivity extends AppCompatActivity {
+
+    private EditText edtNome, edtCpf, edtEmail, edtSenha, edtTelefone;
+    private TextInputLayout tilNome, tilCpf, tilEmail, tilSenha, tilTelefone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+
+        ActionBar ab = getSupportActionBar();
+        ab.hide();
+
+        tilNome = (TextInputLayout) findViewById(R.id.cadastrar_til_nome);
+        tilCpf = (TextInputLayout) findViewById(R.id.cadastrar_til_cpf);
+        tilEmail = (TextInputLayout) findViewById(R.id.cadastrar_til_email);
+        tilSenha = (TextInputLayout) findViewById(R.id.cadastrar_til_senha);
+        tilTelefone = (TextInputLayout) findViewById(R.id.cadastrar_til_telefone);
+
+
+        edtNome = (EditText) findViewById(R.id.cadastrar_edt_nome);
+
+        edtCpf = (EditText) findViewById(R.id.cadastrar_edt_cpf);
+        edtCpf.addTextChangedListener(Mask.insert(Mask.CPF_MASK, edtCpf));
+
+        edtEmail = (EditText) findViewById(R.id.cadastrar_edt_email);
+
+        edtSenha = (EditText) findViewById(R.id.cadastrar_edt_senha);
+
+        edtTelefone = (EditText) findViewById(R.id.cadastrar_edt_telefone);
+        edtTelefone.addTextChangedListener(Mask.insert(Mask.CELULAR_MASK, edtTelefone));
+
+        Button btnCadastrar = (Button) findViewById(R.id.cadastrar_btn_cadastrar);
+        Button btnCancelar = (Button) findViewById(R.id.cadastrar_btn_cancelar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cancelar = new Intent(TelaCadastroActivity.this, MainActivity.class);
+                startActivity(cancelar);
+            }
+        });
+
+    }
+
+    private void submitForm() {
+        if (!validateName()) {
+            return;
+        }
+
+
+        if (!validateCpf()){
+            return;
+        }
+
+        if (!validateEmail()) {
+            return;
+        }
+
+        if (!validatePassword()) {
+            return;
+        }
+
+        if (!validateTelefone()){
+            return;
+        }
+
+        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean validateTelefone() {
+        if (edtTelefone.getText().toString().trim().isEmpty()){
+            tilTelefone.setError(getString(R.string.err_msg_telefone));
+        }
+        return true;
+    }
+
+    private boolean validateName() {
+        if (edtNome.getText().toString().trim().isEmpty()) {
+            tilNome.setError(getString(R.string.err_msg_nome));
+            requestFocus(edtNome);
+            return false;
+        } else {
+            tilNome.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateEmail() {
+        String email = edtEmail.getText().toString().trim();
+
+        if (email.isEmpty() || !isValidEmail(email)) {
+            tilEmail.setError(getString(R.string.err_msg_email));
+            requestFocus(edtEmail);
+            return false;
+        } else {
+            tilEmail.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validatePassword() {
+        if (edtSenha.getText().toString().trim().isEmpty()) {
+            tilSenha.setError(getString(R.string.err_msg_senha));
+            requestFocus(edtSenha);
+            return false;
+        } else {
+            tilSenha.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateCpf(){
+        boolean cpfValido = Validator.validateCPF(edtCpf.getText().toString().trim());
+        if (!cpfValido) {
+            tilCpf.setError(getString(R.string.err_msg_cpf));
+            requestFocus(edtCpf);
+        }else{
+            tilCpf.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+
+
+
+    private static boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+
+}
