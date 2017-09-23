3f7ed50372ca0cd8b0a8a03df8fb69b21a478dc5
Weverton dos Santos Cardoso <re034850@resource.com.br>
2017-09-22 21:29:01 -0300

    atualizacao de telas

--- app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/MenuActivity.java
+++ app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/MenuActivity.java
@@ -2,12 +2,16 @@ package renegociacao.moosegroup.com.br.renegociardividas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class MenuActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
     }
 }
