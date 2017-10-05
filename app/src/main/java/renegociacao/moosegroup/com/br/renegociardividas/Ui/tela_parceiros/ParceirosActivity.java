3484fb589bd17c9dbeae4eb47207a0d527ab65f1
Weverton ¬¬ <weverton.cardoso@hotmail.com>
2017-10-05 10:37:41 -0300

    subindo tela de parceiros

--- app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/Ui/tela_parceiros/ParceirosActivity.java
+++ app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/Ui/tela_parceiros/ParceirosActivity.java
@@ -1,21 +1,26 @@
 package renegociacao.moosegroup.com.br.renegociardividas.Ui.tela_parceiros;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import renegociacao.moosegroup.com.br.renegociardividas.Model.ListaParceiros;
 import renegociacao.moosegroup.com.br.renegociardividas.R;
+import renegociacao.moosegroup.com.br.renegociardividas.RecycleView.RecyclerItemClickListener;
 
 public class ParceirosActivity extends AppCompatActivity {
     private List<ListaParceiros> listaParceiros = new ArrayList<>();
     private RecyclerView recyclerView;
     private ParceirosAdapter parceirosAdapter;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,24 @@ public class ParceirosActivity extends AppCompatActivity {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         recyclerView.setAdapter(parceirosAdapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(context, recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                ListaParceiros parceiro = listaParceiros.get(position);
+                Toast.makeText(getApplicationContext(), parceiro.getNomeFanstasia() + " is selected!", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+
+
+        }));
 
         preparandoListaParceiros();
     }
@@ -40,18 +62,33 @@ public class ParceirosActivity extends AppCompatActivity {
         parceiros = new ListaParceiros("Bradesco", "00000000000", "56229988");
         listaParceiros.add(parceiros);
 
-        parceiros = new ListaParceiros("Bradesco", "00000000000", "56229988");
+        parceiros = new ListaParceiros("Caixa", "111111111111", "99008877");
         listaParceiros.add(parceiros);
 
         parceiros = new ListaParceiros("Bradesco", "00000000000", "56229988");
         listaParceiros.add(parceiros);
 
-        parceiros = new ListaParceiros("Bradesco", "00000000000", "56229988");
+        parceiros = new ListaParceiros("Caixa", "111111111111", "99008877");
         listaParceiros.add(parceiros);
+
 
         parceiros = new ListaParceiros("Bradesco", "00000000000", "56229988");
         listaParceiros.add(parceiros);
+
+        parceiros = new ListaParceiros("Caixa", "111111111111", "99008877");
+        listaParceiros.add(parceiros);
+
+
+        parceiros = new ListaParceiros("Bradesco", "00000000000", "56229988");
+        listaParceiros.add(parceiros);
+
+
+        parceiros = new ListaParceiros("Caixa", "111111111111", "99008877");
+        listaParceiros.add(parceiros);
+
 
         parceirosAdapter.notifyDataSetChanged();
     }
+
+
 }
--- app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/Ui/tela_parceiros/RecyclerTouchListener.java
+++ app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/Ui/tela_parceiros/RecyclerTouchListener.java
@@ -1,14 +1,17 @@
-package renegociacao.moosegroup.com.br.renegociardividas.RecycleView;
+package renegociacao.moosegroup.com.br.renegociardividas.Ui.tela_parceiros;
+
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
+import renegociacao.moosegroup.com.br.renegociardividas.RecycleView.RecyclerItemClickListener;
 
-public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
 
-    private OnItemClickListener mListener;
+public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+    private RecyclerItemClickListener.OnItemClickListener mListener;
 
     public interface OnItemClickListener {
         public void onItemClick(View view, int position);
@@ -18,7 +21,7 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
 
     GestureDetector mGestureDetector;
 
-    public RecyclerItemClickListener(Context context, final RecyclerView recyclerView, OnItemClickListener listener) {
+    public RecyclerTouchListener(Context context, final RecyclerView recyclerView, RecyclerItemClickListener.OnItemClickListener listener) {
         mListener = listener;
         mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
