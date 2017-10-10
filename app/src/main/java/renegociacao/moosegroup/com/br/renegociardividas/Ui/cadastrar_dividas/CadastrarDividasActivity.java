88ce00f54aae6fbe7a12b79d2c980e67412033ef
Weverton ¬¬ <weverton.cardoso@hotmail.com>
2017-10-10 17:05:41 -0300

    Implementando tela cadastrar Dividas

--- app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/Ui/cadastrar_dividas/CadastrarDividasActivity.java
+++ app/src/main/java/renegociacao/moosegroup/com/br/renegociardividas/Ui/cadastrar_dividas/CadastrarDividasActivity.java
@@ -1,17 +1,21 @@
 package renegociacao.moosegroup.com.br.renegociardividas.Ui.cadastrar_dividas;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import renegociacao.moosegroup.com.br.renegociardividas.Model.DividaModel;
 import renegociacao.moosegroup.com.br.renegociardividas.R;
 import renegociacao.moosegroup.com.br.renegociardividas.Ui.dividas.DividasActivity;
 
 
 public class CadastrarDividasActivity extends AppCompatActivity {
 
+    private TextView txtTitulo, txtDescricao, txtValor, txtEmpresa;
+    private Button btnCadastrar, btnCancelar;
 
 
     @Override
@@ -21,6 +25,33 @@ public class CadastrarDividasActivity extends AppCompatActivity {
 
         getSupportActionBar().setElevation(0);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        loadComponents();
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CadastrarDividasActivity.this, DividasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void loadComponents() {
+        txtTitulo = (TextView) findViewById(R.id.cadastrarDividas_edt_titulo);
+        txtDescricao = (TextView) findViewById(R.id.cadastrarDividas_edt_descricao);
+        txtValor = (TextView) findViewById(R.id.cadastrarDividas_edt_valor);
+        txtEmpresa = (TextView) findViewById(R.id.cadastrarDividas_edt_empresa);
+        btnCadastrar = (Button) findViewById(R.id.cadastrarDividas_btn_cadastrar);
+        btnCancelar = (Button) findViewById(R.id.cadastrarDividas_btn_cancelar);
     }
 
     @Override
